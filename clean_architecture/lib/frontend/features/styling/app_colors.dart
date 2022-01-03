8a6c646c467b0226e289d177c6f4d0fdc031b930
jbhertzog <jamesbarryhertzog@gmail.com>
2022-01-03 03:03:32 +0200

    Removed unused color

--- clean_architecture/lib/frontend/features/styling/app_colors.dart
+++ clean_architecture/lib/frontend/features/styling/app_colors.dart
@@ -4,6 +4,5 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color darkBlue = Color(0xff032333);
-  static const Color lightBlue = Color(0xff008bd0);
   static const Color red = Color(0xffde0b1c);
 }
